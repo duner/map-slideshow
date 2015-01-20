593cd14c5e5abd72635c7eb6b2bd662204dc05a8
duner <asduner@gmail.com>
2015-01-19 22:33:00 -0600

    fix the zoom (and afghanistan)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,37 +4,37 @@ $(document).ready(function(){
   var height = $("#map").height();
   var active = d3.select(null);
 
-  var projection = d3.geo.vanDerGrinten3()
-      .scale(350)
-      .translate([width / 2, height / 1.5])
-      .precision(0.1);
+  var projection = d3.geo.mercator()
+    .scale(350)
+    .translate([width / 2, height / 1.5])
+    .precision(0.1);
 
   var path = d3.geo.path()
-      .projection(projection);
+    .projection(projection);
 
   var graticule = d3.geo.graticule();
 
   var svg = d3.select("#map").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
 
   svg.append("defs").append("path")
-      .datum({type: "Sphere"})
-      .attr("id", "sphere")
-      .attr("d", path);
+    .datum({type: "Sphere"})
+    .attr("id", "sphere")
+    .attr("d", path);
 
   svg.append("use")
-      .attr("class", "stroke")
-      .attr("xlink:href", "#sphere");
+    .attr("class", "stroke")
+    .attr("xlink:href", "#sphere");
 
   svg.append("use")
-      .attr("class", "fill")
-      .attr("xlink:href", "#sphere");
+    .attr("class", "fill")
+    .attr("xlink:href", "#sphere");
 
   svg.append("path")
-      .datum(graticule)
-      .attr("class", "graticule");
-      // .attr("d", path);
+    .datum(graticule)
+    .attr("class", "graticule");
+    // .attr("d", path);
 
   var zoom = d3.behavior.zoom()
     .translate([0, 0])
@@ -42,23 +42,33 @@ $(document).ready(function(){
     .scaleExtent([1, 8])
     .on("zoom", zoomed);
 
+  var g = svg.append("g");
+
   svg
     .call(zoom) // delete this line to disable free zooming
     .call(zoom.event);
 
   d3.json("/scripts/world-50m.json", function(error, world) {
 
-    svg.selectAll("path")
-      .data(topojson.feature(world, world.objects.countries).features)
+    countries = topojson.feature(world, world.objects.countries)
+    console.log(countries);
+
+    g.selectAll("path")
+      .data(countries.features)
     .enter().append("path")
       .attr("class", "country")
+      .attr("data-id", function(d) {
+        if (d.id == 4) { console.log(d); }
+        console.log(d.id)
+        return d.id;
+        })
       .attr("d", path)
       .on("click", clicked);
 
-    svg.append("path", ".graticule")
-        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-        .attr("class", "boundary")
-        .attr("d", path);
+    g.append("path", ".graticule")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .attr("class", "boundary")
+      .attr("d", path);
   });
 
   function clicked(d) {
@@ -67,16 +77,16 @@ $(document).ready(function(){
     active = d3.select(this).classed("active", true);
 
     var bounds = path.bounds(d),
-        dx = bounds[1][0] - bounds[0][0],
-        dy = bounds[1][1] - bounds[0][1],
-        x = (bounds[0][0] + bounds[1][0]) / 2,
-        y = (bounds[0][1] + bounds[1][1]) / 2,
-        scale = .9 / Math.max(dx / width, dy / height),
-        translate = [width / 2 - scale * x, height / 2 - scale * y];
+      dx = bounds[1][0] - bounds[0][0],
+      dy = bounds[1][1] - bounds[0][1],
+      x = (bounds[0][0] + bounds[1][0]) / 2,
+      y = (bounds[0][1] + bounds[1][1]) / 2,
+      scale = .9 / Math.max(dx / width, dy / height),
+      translate = [width / 2 - scale * x, height / 2 - scale * y];
 
     svg.transition()
-        .duration(750)
-        .call(zoom.translate(translate).scale(scale).event);
+      .duration(750)
+      .call(zoom.translate(translate).scale(scale).event);
   }
 
   function reset() {
@@ -84,13 +94,13 @@ $(document).ready(function(){
     active = d3.select(null);
 
     svg.transition()
-        .duration(750)
-        .call(zoom.translate([0, 0]).scale(1).event);
+      .duration(750)
+      .call(zoom.translate([0, 0]).scale(1).event);
   }
 
   function zoomed() {
-    svg.style("stroke-width", 1.5 / d3.event.scale + "px");
-    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    g.style("stroke-width", 1.5 / d3.event.scale + "px");
+    g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
   }
 
   // If the drag behavior prevents the default click,
@@ -98,7 +108,6 @@ $(document).ready(function(){
   function stopped() {
     if (d3.event.defaultPrevented) d3.event.stopPropagation();
   }
-
 
   d3.select(self.frameElement).style("height", height + "px");
 
